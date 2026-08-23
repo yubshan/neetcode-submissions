4922cd80218ad3bd85b622d5b9e7cb5e903f1835
yubshan <129408332+yubshan@users.noreply.github.com>
2026-08-23 16:14:09 +0545

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,33 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> memory;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+       memory[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(memory.find(key) == memory.end()) return "";
+
+        const auto& history = memory[key];
+        int l = 0 , r = history.size()-1;
+        string res = "";
+
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            const auto& value = history[mid];
+            if(value.first <= timestamp){
+                res = history[mid].second;
+                l = mid + 1;
+            }else{
+                r = mid  - 1;
+            }
+        }
+        
+       return res;
+    }
+};
