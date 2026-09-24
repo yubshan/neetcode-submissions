3933277ddb45aeb007605918eb9526c788dce895
yubshan <129408332+yubshan@users.noreply.github.com>
2026-09-24 09:33:48 +0545

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,23 @@
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>>minHeap;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(auto num : nums){
+            minHeap.push(num);
+            if(minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size()>k){
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
